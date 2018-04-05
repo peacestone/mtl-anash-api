7e98e346c12c49d23cc78c29c6f5741b21a574b9
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2018-04-04 23:20:54 -0400

    add cors and logger middleware

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express')
-
 const people = require('./seeds')
-
 const app = express()
+const cors = require('cors')
+
+let counter = 0
+
+
+
+
+const logger = (req, res, next) => {
+  console.log(`req number ${++counter}`)
+  next()
+}
+
+app.use(logger, cors({
+  origin: 'http://localhost:3000'
+}))
 
 app.get('/api/people', function (req, res) {
     res.json(people)
